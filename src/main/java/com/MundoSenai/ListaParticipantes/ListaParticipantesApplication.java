8ac92001757fde09252a825e3b0e3a04d4e2ed0b
Bruno de Faria <brunofaria@MacBook-Air-de-Bruno.local>
2023-07-28 19:12:03 -0300

    Initial commit

---
+++ src/main/java/com/MundoSenai/ListaParticipantes/ListaParticipantesApplication.java
@@ -0,0 +1,13 @@
+package com.MundoSenai.ListaParticipantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaParticipantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaParticipantesApplication.class, args);
+	}
+
+}
